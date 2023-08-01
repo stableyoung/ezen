ee8544d4a8c6136339b085acdca7bcf5c14d9ab4
stableyoung <stableyoung@42-4.org>
2023-08-02 02:49:25 +0900

    2에서 100까지 숫자중 소수를 구하는 예제

--- src/ch040/Prime.java
+++ src/ch040/Prime.java
@@ -4,16 +4,29 @@ package src.ch040;
 public class Prime {
     public static void main(String[] args) {
         for(int i = 2; i <= 100; i++) {
-            boolean isPrime = true;
-            for(int j = 2; j < i; j++) {
-                if(i % j == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
+
+//            boolean isPrime = true;
+//            for(int j = 2; j < i; j++) {
+//                if(i % j == 0) {
+//                    isPrime = false;
+//                    break;
+//                }
+//            }
+            boolean isPrime = Prime.checkPrime(i);
             if(isPrime) {
                 System.out.println(i);
             }
         }
+    }
+
+    static boolean checkPrime(int number) {
+        boolean isPrime = true;
+        for(int i = 2; i < number; i++) {
+            if(number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        return isPrime;
     }
 }
