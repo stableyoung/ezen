f927111a684fd681120bfea79401deccc7002323
stableyoung <stableyoung@naver.com>
2023-08-01 11:24:26 +0900

    제3강

---
+++ src/ch030/Main.java
@@ -0,0 +1,23 @@
+package src.ch030;
+
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+
+        int difference = num1 - num2;
+        System.out.println("Difference: " + difference);
+
+        int product = num1 * num2;
+        System.out.println("Product: " + product);
+
+        int quotient = num1 / num2;
+        System.out.println("Quotient: " + quotient);
+
+        int remainder = num1 % num2;
+        System.out.println("Remainder: " + remainder);
+    }
+}
