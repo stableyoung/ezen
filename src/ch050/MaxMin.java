0104fd92f96a0b00f46e29cf1afff97bbf046241
stableyoung <stableyoung@42-4.org>
2023-08-02 03:57:00 +0900

    구구단, 최소최대

---
+++ src/ch050/MaxMin.java
@@ -0,0 +1,17 @@
+package src.ch050;
+
+
+public class MaxMin {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int min = array[0];
+        int max = array[0];
+
+        for(int i = 1; i < array.length; i++) {
+            if(array[i] < min) min = array[i];
+            if(array[i] > max) max = array[i];
+        }
+
+        System.out.println("Min: " + min + ", Max: " + max);
+    }
+}
