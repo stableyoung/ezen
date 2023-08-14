11f2420e583dde5728e8911ac05c7b88d1a5b10c
stableyoung <stableyoung@42-4.org>
2023-08-14 18:55:00 +0900

    수정 보완

--- src/ch070/readme.txt
+++ src/ch070/readme.txt
@@ -1,3 +1,5 @@
 1. MainObject : Person 클래스를 정의하고 호출하는 예제
 
 2. AnimalDemo : Animal 클래스를 정의하고 호출하는 예제
+
+3. MainCar : Car 클래스를 정의하고 호출하는 예제
---
+++ src/exercise/ch070/MainCar.java
@@ -0,0 +1,41 @@
+package src.exercise.ch070;
+
+public class MainCar {
+    public static void main(String[] args) {
+       Car myCar = new Car();
+         myCar.name = "포르쉐";
+         myCar.color = "빨강";
+
+         myCar.speedUp();
+         myCar.speedUp();
+         myCar.speedDown();
+
+         myCar.print();
+         myCar.stop();
+         myCar.print();
+    }
+}
+
+class Car {
+    String name;
+    String color;
+    int speed;
+
+    void speedUp() {
+        speed += 10;
+    }
+
+    void speedDown() {
+        speed -= 10;
+    }
+
+    void stop() {
+        speed = 0;
+    }
+
+    void print() {
+        System.out.println("차종: " + name);
+        System.out.println("색상: " + color);
+        System.out.println("속도: " + speed);
+    }
+}
