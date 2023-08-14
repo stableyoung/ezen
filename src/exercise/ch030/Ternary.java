46a544b468e565447e9050d6a89c24c5e0603707
stableyoung <stableyoung@42-4.org>
2023-08-14 13:25:00 +0900

    삼항연산자 수정

--- src/exercise/ch030/Ternary.java
+++ src/exercise/ch030/Ternary.java
@@ -6,7 +6,7 @@ public class Ternary {
         int b = 20;
 
         // a가 b보다 큰지 확인하고, 크면 "a가 더 큽니다"를, 그렇지 않으면 "b가 더 큽니다"를 출력
-        String result = (a > b) ? "a가 더 큽니다" : "b가 더 큽니다";
+        String result = (a > b) ? "a가 더 큽니다" : "b가 더 큽니다"; // if else 문을 한 줄로 표현 가능
         System.out.println(result);  // b가 더 큽니다
     }
 }
